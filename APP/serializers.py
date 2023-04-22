da3d2dfb4088e269e0fa193032ecdff9cdb78d5e
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-04-23 01:30:26 +0530

    Step-11:Successfull nested object crreation!!

--- APP/serializers.py
+++ APP/serializers.py
@@ -8,17 +8,10 @@ class ChildSerializer(serializers.ModelSerializer):
         model = child
         fields = ["child_name"]
 
-    # def create(self, validated_data):
-    #     return validated_data
-
-
 class CowSerializer(serializers.ModelSerializer):
     class Meta:
         model = cow
         fields = ["cow_name"]
-
-    # def create(self, validated_data):
-    #     return validated_data
 
 
 class SheapSerializer(serializers.ModelSerializer):
@@ -26,24 +19,17 @@ class SheapSerializer(serializers.ModelSerializer):
         model = sheap
         fields = ["sheap_name"]
 
-    # def create(self, validated_data):
-    #     return validated_data
-
 
 class GoatSerializer(serializers.ModelSerializer):
     class Meta:
         model = goat
         fields = ["goat_name"]
 
-    # def create(self, validated_data):
-    #     return validated_data
-
-
 class FamilySerializer(serializers.ModelSerializer):
-    name_child = ChildSerializer()
-    name_cow = CowSerializer()
-    name_sheap = SheapSerializer()
-    name_goat = GoatSerializer()
+    child_names = ChildSerializer()
+    cow_names = CowSerializer()
+    sheap_names = SheapSerializer()
+    goat_names = GoatSerializer()
 
     class Meta:
         model = family
@@ -51,38 +37,37 @@ class FamilySerializer(serializers.ModelSerializer):
             "Farmer",
             "Wife",
             "children",
-            "name_child",
+            "child_names",
             "cows",
-            "name_cow",
+            "cow_names",
             "sheaps",
-            "name_sheap",
+            "sheap_names",
             "goats",
-            "name_goat",
+            "goat_names",
         ]
         validators = [
             UniqueTogetherValidator(queryset=family.objects.all(), fields=["Farmer"])
         ]
-
+    
     def create(self, validated_data):
-        childs = validated_data.pop("name_child")
-        cows = validated_data.pop("name_cow")
-        sheaps = validated_data.pop("name_sheap")
-        goats = validated_data.pop("name_goat")
-        child.objects.create(**childs)
-        cow.objects.create(**cows)
-        sheap.objects.create(**sheaps)
-        goat.objects.create(**goats)
-        print(type(validated_data))
+        child_data = validated_data.pop('child_names')
+        cow_data = validated_data.pop('cow_names')
+        sheap_data = validated_data.pop('sheap_names')
+        goat_data = validated_data.pop('goat_names')
+
+        # Create child, cow, sheap and goat objects
+        child_obj = child.objects.create(**child_data)
+        cow_obj = cow.objects.create(**cow_data)
+        sheap_obj = sheap.objects.create(**sheap_data)
+        goat_obj = goat.objects.create(**goat_data)
+
+        # Create family object with the nested objects
         fam = family.objects.create(
-            child_names=childs,
-            cow_names=cows,
-            sheap_names=sheaps,
-            goat_names=goats,
+            child_names=child_obj,
+            cow_names=cow_obj,
+            sheap_names=sheap_obj,
+            goat_names=goat_obj,
             **validated_data
         )
-        # fam=family.objects.create(child_names=childs,
-        #                           cow_names=cows,
-        #                           sheap_names=sheaps,
-        #                           goat_names=goats,
-        #                           **validated_data)
         return fam
+
