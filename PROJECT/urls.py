68345490da52ecbc102ff3e16f4927ede22f0555
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-07-19 15:21:00 +0530

    logic automatic route changing added

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.shortcuts import redirect
+from django.urls import path, include
+
+def redirect_to_api(request):
+    return redirect('api/')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/",include('api.urls'))
+    path("", redirect_to_api, name="redirect_to_api"),
+    path("api/", include('api.urls')),
 ]
